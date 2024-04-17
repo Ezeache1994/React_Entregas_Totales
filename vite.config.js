a3a8be6122e9fd16dd25782d34b6d20bd80f74ba
Ezequiel <ezea1994@hotmail.com>
2024-04-17 17:35:49 -0300

    intento de render pages github

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://Ezeache1994.github.io/React_Entregas_Totales"
 })
+
+
+
+Ezeache1994
